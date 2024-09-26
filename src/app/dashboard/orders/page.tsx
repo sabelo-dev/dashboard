a80411c17a68715935b362b2538e3b5d888b36e7
sabelom <zwidekasabelo@gmail.com>
2024-09-26 06:07:14 +0200

    dashboard update

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -78,9 +78,6 @@ import {
 } from "@/components/ui/tooltip"
 import Nav from "@/components/Nav"
 
-export const description =
-  "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information."
-
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
