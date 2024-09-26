03216ee4f42aac402c2d7de6e32084a6e62a3b6a
sabelom <zwidekasabelo@gmail.com>
2024-09-26 06:21:17 +0200

    dashboard update

--- src/app/dashboard/products/edit/page.tsx
+++ src/app/dashboard/products/edit/page.tsx
@@ -70,9 +70,6 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
-export const description =
-  "A product edit page. The product edit page has a form to edit the product details, stock, product category, product status, and product images. The product edit page has a sidebar navigation and a main content area. The main content area has a form to edit the product details, stock, product category, product status, and product images. The sidebar navigation has links to product details, stock, product category, product status, and product images."
-
 export default function Edit() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
