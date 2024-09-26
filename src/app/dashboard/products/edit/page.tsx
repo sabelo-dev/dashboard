14b33f7eb6c6976ff8a10e003c4263bc9dea7f66
sabelom <zwidekasabelo@gmail.com>
2024-09-26 06:15:54 +0200

    dashboard update

--- src/app/dashboard/products/edit/page.tsx
+++ src/app/dashboard/products/edit/page.tsx
@@ -73,7 +73,7 @@ import {
 export const description =
   "A product edit page. The product edit page has a form to edit the product details, stock, product category, product status, and product images. The product edit page has a sidebar navigation and a main content area. The main content area has a form to edit the product details, stock, product category, product status, and product images. The sidebar navigation has links to product details, stock, product category, product status, and product images."
 
-export function Dashboard() {
+export default function Edit() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
