5d56d658c4e763a934b8615b689ec0bbab6c3319
sabelom <zwidekasabelo@gmail.com>
2024-09-26 06:27:20 +0200

    dashboard updated

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -62,9 +62,6 @@ import {
 import Nav from "@/components/Nav"
 
 
-export const description =
-  "A products dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. It displays a list of products in a table with actions."
-
 export default function Products() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
