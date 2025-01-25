3fd81f6fd62d4c21c1378311cd684d94ce677710
sabelom <zwidekasabelo@gmail.com>
2025-01-25 11:01:30 +0200

    fixed upload req type

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,7 @@
 import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import uniqid from 'uniqid';
 
-export async function POST(req) {
+export async function POST(req: any) {
   const data =  await req.formData();
   if (data.get('file')) {
     // upload the file
